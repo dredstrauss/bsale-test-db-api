027b8859ee007f7f0539bf5f343622be42fc7353
Dred Strauss <dred@dredstrauss.com>
2022-01-10 12:24:36 -0300

    agregada ruta base

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const url = require('url');
+const text = require('./lang.json');
+
+let lang = "esp";
+const setLang = (l) => { if (l === "esp" || l === "eng") { lang = lg } };
+
+const versionsList = require('./modules/VersionsList');
+const getAbsUrl = require('./modules/GetAbsUrl');
 
 const app = express();
-
-const text = require('./lang.json'); let lang;
-
 const server = (LANG,PORT) => {
     lang = LANG;
     app.listen(PORT, () => console.log(`${text.server.listening[lang]}: ${PORT}`));
-}
+};
+
+const versions = ["v1"];
+
+app.get('/', (req,res) => {
+    let queries = url.parse(req.url,true).query;
+    setLang(queries.lang);
+
+    res.status(200);
+    res.json(versionsList(versions,getAbsUrl(req,res),text.routes.versions[lang]));
+});
 
 module.exports = { server };
