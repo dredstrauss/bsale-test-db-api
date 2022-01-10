7c1574a4a3c26f0a16a261d7dadcf26c7d977cba
Dred Strauss <dred@dredstrauss.com>
2022-01-10 19:13:57 -0300

    query all agregada

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const url = require('url');
 const text = require('./lang.json');
 
@@ -8,9 +9,14 @@ const versionsList = require('./modules/VersionsList');
 const getAbsUrl = require('./modules/GetAbsUrl');
 
 const app = express();
+
+const dbConfig = process.env.DATABASE_URI;
+let connection;
+
 const server = (LANG,PORT) => {
     lang = LANG;
     app.listen(PORT, () => console.log(`${text.server.listening[lang]}: ${PORT}`));
+    connection = mysql.createConnection(dbConfig);
 };
 
 const versions = ["v1"];
@@ -24,6 +30,21 @@ app.get('/', (req,res) => {
 
     res.status(200);
     res.json(versionsList(versions,getAbsUrl(req,res),text.routes.versions[lang]));
+});
+
+// API v1
+
+app.get('/v1/all', async(req,res) => {
+    queryLang(req);
+
+    const SQLQuery = `SELECT category.name AS category, p.id AS id, p.name AS name, p.url_image AS img, p.price AS price, p.discount AS off FROM product AS p JOIN category ON p.category = category.id;`;
+    
+    connection.query(SQLQuery, (error, results, fields) => {
+        if (error) throw error;
+        res.json(results)
+        connection.end();
+    });
+
 });
 
 module.exports = { server };
